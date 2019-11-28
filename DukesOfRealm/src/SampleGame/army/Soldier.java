eb1470ca3285fe6859409c49427112f9f444792f
Théo Dupont <theo.dupont@etu.u-bordeaux.fr>
2019-11-28 15:16:21 +0100

    Modifs Soldier.java et ajout Main.java

--- DukesOfRealm/src/SampleGame/army/Soldier.java
+++ DukesOfRealm/src/SampleGame/army/Soldier.java
@@ -11,7 +11,36 @@ public class Soldier {
 	public Soldier() {}
 	
 	public void executeOrder66(Castle target) {
-		
+		int moves = speed;
+		int c_x = target.getX();
+		if(c_x != tile_x) {
+			int d_x = c_x - tile_x;
+			int d_ux = d_x<0?-1:1;
+			while(c_x != tile_x && moves>0) {
+				tile_x =+ d_ux;
+				moves --;
+			}			
+		}
+		int c_y = target.getY();
+		if(c_y != tile_y) {
+			int d_y = c_y - tile_y;
+			int d_uy = d_y<0?-1:1;
+			while(c_y != tile_y && moves>0) {
+				tile_y =+ d_uy;
+				moves --;
+			}			
+		}
+		if(c_x == tile_x && c_y == tile_y) {
+			arrivedCastle(target);
+		}
+	}
+	
+	public void arrivedCastle(Castle target) {
+		if(target.getDuke_owner()==this.duke_owner) {
+			target.addToArmy(this);
+		}else {
+			target.getAttacked(this);
+		}
 	}
 
 	public String getName() {
