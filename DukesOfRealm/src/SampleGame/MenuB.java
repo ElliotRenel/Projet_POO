4f8845c55bd2e0466fc2d96e4d42b2ca87bf8e93
theod <theod@192.168.0.15>
2020-01-06 23:26:02 +0100

    Avancement things

--- DukesOfRealm/src/SampleGame/MenuB.java
+++ DukesOfRealm/src/SampleGame/MenuB.java
@@ -2,23 +2,35 @@ package SampleGame;
 
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-
+/**
+ * 
+ * @author theod
+ *
+ *MenuB allows to display a menu bar at the top of the game window
+ */
 public class MenuB extends MenuBar {
-
+	
+	/**
+	 * Constructor for MenuB.
+	 * rounds, save and load are 3 different menus.
+	 */
 	public MenuB() {
 		super();
-		this.setId("Bob");
 		
 		Menu rounds = new Menu();
 		rounds.setText("Rounds: " + Settings.NB_CURRENT_ROUND);
 		Menu save = new Menu("Save");
 		Menu load = new Menu("Load");
-		Menu pause = new Menu("Pause");
 		
-		this.getMenus().addAll(rounds, save, load, pause);
+		this.getMenus().addAll(rounds, save, load);
 		this.setPrefSize(Settings.SCENE_WIDTH, 20);
 	}
 	
+	
+	/**
+	 * Allows the menu rounds to be updated by the game.
+	 * @param rounds the number of rounds to be printed.
+	 */
 	public void updateMenuBar(String rounds) {
 		Menu roundCount = new Menu("Round: " + rounds);
 		this.getMenus().set(0, roundCount);
